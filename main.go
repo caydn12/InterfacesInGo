2f24e2915afc6173c9f16ef1ca6a558b62c512b0
Caydn12 <Caydnlofton@gmail.com>
2018-08-26 12:33:38 -0700

    Created two chat bots, one that returns an english greeting and one that returns a spanish greeting. Defined an interface for a chatbot, and created a function (printGretting()) that takes a chatbot as an argument. Confirmed that the english and spanish bots both implemented the chatbot's interface, and passed them as arguments to the printGretting() function

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+type chatBot interface {
+	getGreeting() string
+}
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func (englishBot) getGreeting() string {
+	return "Hello"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola"
+}
+
+func printGreeting(cb chatBot) {
+	fmt.Println(cb.getGreeting())
+}
